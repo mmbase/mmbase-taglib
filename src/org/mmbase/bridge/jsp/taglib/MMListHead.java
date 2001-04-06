133a9b8daedaef7b7005b35ec7d86fc8fbef9015
Kees Jongenburger <github@keesj.dds.nl>
2001-04-06 09:16:19 +0000

    updated docs and indenting

--- src/org/mmbase/bridge/jsp/taglib/MMListHead.java
+++ src/org/mmbase/bridge/jsp/taglib/MMListHead.java
@@ -5,17 +5,17 @@ import javax.servlet.jsp.tagext.*;
 
 
 /**
- * conditional sub tag for list. content enclosed in the head tag
- * will only be displayed if the current list item is the first item
- * in the list
- * @author Kees Jongenburger
- **/
+* conditional sub tag for list. content enclosed in the head tag
+* will only be displayed if the current list item is the first item
+* in the list therefore the head tag uses MMList.isFirst();
+* @author Kees Jongenburger
+**/
 public class MMListHead extends BodyTagSupport{
-
-       public int doStartTag() throws JspException{
+	
+	public int doStartTag() throws JspException{
 		try {
-		   MMList mmList = (MMList)this.findAncestorWithClass((Tag)this,Class.forName("org.mmbase.bridge.jsp.taglib.MMList"));
-		
+			MMList mmList = (MMList)this.findAncestorWithClass((Tag)this,Class.forName("org.mmbase.bridge.jsp.taglib.MMList"));
+			
 			if (mmList == null)
 				return SKIP_BODY;
 			if (mmList.isFirst())
@@ -25,16 +25,14 @@ public class MMListHead extends BodyTagSupport{
 		}
 		return SKIP_BODY;
 	}
-  public int doEndTag() throws JspException
-  {
-    try
-    {
-      if(bodyContent != null)
-        bodyContent.writeOut(bodyContent.getEnclosingWriter());
-    } catch(java.io.IOException e)
-    {
-      throw new JspException("IO Error: " + e.getMessage());
-    }
-    return EVAL_PAGE;
-  }
+	
+	public int doEndTag() throws JspException{
+		try{
+			if(bodyContent != null)
+				bodyContent.writeOut(bodyContent.getEnclosingWriter());
+		} catch(java.io.IOException e){
+			throw new JspException("IO Error: " + e.getMessage());
+		}
+		return EVAL_PAGE;
+	}
 }
--- src/org/mmbase/bridge/jsp/taglib/MMListTail.java
+++ src/org/mmbase/bridge/jsp/taglib/MMListTail.java
@@ -5,35 +5,34 @@ import javax.servlet.jsp.tagext.*;
 
 
 /**
- * conditional sub tag for list. content enclosed in the head tag
- * will only be displayed if the current list item is the last item
- * in the list
- * @author Kees Jongenburger
- **/
+* conditional sub tag for list. content enclosed in the tail tag
+* will only be displayed if the current list item is the last item
+* in the list
+* @author Kees Jongenburger
+**/
 public class MMListTail extends BodyTagSupport{
+	
 	public int doStartTag() throws JspException{
 		try {
 			MMList mmList = (MMList)this.findAncestorWithClass((Tag)this,Class.forName("org.mmbase.bridge.jsp.taglib.MMList"));
-		if (mmList == null)
-			return SKIP_BODY;
-		if (mmList.isLast())
-			return EVAL_BODY_TAG;
+			if (mmList == null)
+				return SKIP_BODY;
+			if (mmList.isLast())
+				return EVAL_BODY_TAG;
 		}  catch(Exception e) {
 			return SKIP_BODY;
 		} 
 		return SKIP_BODY;
 	}
-  public int doEndTag() throws JspException
-  {
-    try
-    {
-      if(bodyContent != null)
-        bodyContent.writeOut(bodyContent.getEnclosingWriter());
-    } catch(java.io.IOException e)
-    {
-      throw new JspException("IO Error: " + e.getMessage());
-    }
-    return EVAL_PAGE;
-  }
+	
+	public int doEndTag() throws JspException{
+		try{
+			if(bodyContent != null)
+				bodyContent.writeOut(bodyContent.getEnclosingWriter());
+		} catch(java.io.IOException e){
+			throw new JspException("IO Error: " + e.getMessage());
+		}
+		return EVAL_PAGE;
+	}
 
 }
