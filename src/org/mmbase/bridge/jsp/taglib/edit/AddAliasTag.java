09c6b57e82842adbfdd8d7fe212f31134a643018
Michiel Meeuwissen <git@meeuw.org>
2001-06-12 19:15:49 +0000

    michiel: a few new edit tags -cancel and commit for use in <mm:transaction> -addAlias to use in a NodeProviderTag.

---
+++ src/org/mmbase/bridge/jsp/taglib/edit/AddAliasTag.java
@@ -0,0 +1,51 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib.edit;
+
+import javax.servlet.jsp.JspTagException;
+
+import org.mmbase.bridge.Node;
+import org.mmbase.bridge.jsp.taglib.NodeReferrerTag;
+
+/**
+* To call the method addAlias from Node.
+* 
+* @author Michiel Meeuwissen
+*/
+public class AddAliasTag extends NodeReferrerTag {    
+
+    private String parameter = null;
+
+    public void setParameter(String p) {
+        parameter = p;
+    }
+    /**
+    * Add the alias.
+    **/
+    public int doAfterBody() throws JspTagException {        
+        // search the node:
+        Node node = findNodeProvider().getNodeVar();
+        
+        // alias name is in the body if no parameter is given
+        String alias;
+        if (parameter == null) {
+            alias = bodyContent.getString();
+        } else {
+            alias =  pageContext.getRequest().getParameter(parameter);
+        }
+
+        if (alias != null) {
+            if (! "".equals(alias)) {
+                node.addAlias(alias);
+            }
+        }
+        return SKIP_BODY;
+    }
+}
---
+++ src/org/mmbase/bridge/jsp/taglib/edit/CancelTag.java
@@ -0,0 +1,36 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib.edit;
+
+import javax.servlet.jsp.JspTagException;
+
+import org.mmbase.bridge.jsp.taglib.CloudReferrerTag;
+import org.mmbase.bridge.Transaction;
+
+/**
+* This tag can be used inside a transaction tag, to cancel it.
+*
+* @author Michiel Meeuwissen 
+**/
+
+public class CancelTag extends CloudReferrerTag { 
+    // perhaps it would be nicer to extend CloudReferrer to TransactionReferrer first.
+
+    void setTransaction(String t) {
+        setCloud(t);
+    }
+
+    public int doStartTag() throws JspTagException{
+        // find the parent transaction:
+        Transaction trans = (Transaction) getCloudProviderVar();
+        trans.cancel();
+        return SKIP_BODY;    
+    }    
+}
