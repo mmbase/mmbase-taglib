473b39a230b97454e66c0bea4716821097e867c6
Michiel Meeuwissen <git@meeuw.org>
2001-06-22 21:00:39 +0000

    michiel: An ImportTag as well. To put things in the context.

---
+++ src/org/mmbase/bridge/jsp/taglib/ImportTag.java
@@ -0,0 +1,33 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib;
+
+import javax.servlet.jsp.tagext.BodyTagSupport;
+import javax.servlet.jsp.tagext.BodyContent;
+import javax.servlet.jsp.JspTagException;
+
+/**
+* The importtag takes its body, and writes it to the context.
+* 
+* @author Michiel Meeuwissen
+*/
+public class ImportTag extends CloudReferrerTag {
+
+    private String key = null;
+    public void setKey(String k) {
+        key = k;
+    }
+    
+    public int doAfterBody() throws JspTagException{
+        findCloudProvider().register(key, bodyContent.getString());
+        return SKIP_BODY;
+    }
+
+}
