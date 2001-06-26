7439413932868faf54cbfe10a16d3bf267c18d59
Michiel Meeuwissen <git@meeuw.org>
2001-06-26 20:06:42 +0000

    michiel: Tag to remove something from a Context

---
+++ src/org/mmbase/bridge/jsp/taglib/RemoveTag.java
@@ -0,0 +1,28 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib;
+
+import javax.servlet.jsp.tagext.BodyTagSupport;
+import javax.servlet.jsp.tagext.BodyContent;
+import javax.servlet.jsp.JspTagException;
+
+/**
+* Remove an object from the Context.
+* 
+* @author Michiel Meeuwissen
+*/
+public class RemoveTag extends ImportTag {
+
+    public int doAfterBody() throws JspTagException{
+        findCloudProvider().unRegister(key);
+        return SKIP_BODY;
+    }
+
+}
