057a22401509b0ac20f26cf7ae121a96a2bd92c4
Michiel Meeuwissen <git@meeuw.org>
2001-06-26 20:05:52 +0000

    michiel: key

--- src/org/mmbase/bridge/jsp/taglib/ImportTag.java
+++ src/org/mmbase/bridge/jsp/taglib/ImportTag.java
@@ -9,9 +9,11 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.bridge.jsp.taglib;
 
-import javax.servlet.jsp.tagext.BodyTagSupport;
 import javax.servlet.jsp.tagext.BodyContent;
 import javax.servlet.jsp.JspTagException;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
 
 /**
 * The importtag takes its body, and writes it to the context.
@@ -20,8 +22,17 @@ import javax.servlet.jsp.JspTagException;
 */
 public class ImportTag extends CloudReferrerTag {
 
-    public int doAfterBody() throws JspTagException{
-        findCloudProvider().register(getId(), bodyContent.getString());
+    private static Logger log = Logging.getLoggerInstance(ImportTag.class.getName()); 
+
+    protected String key = null;
+    
+    public void setKey(String k) {
+        key = k;
+    }
+
+    public int doAfterBody() throws JspTagException{        
+        log.debug("Setting " + key + " to " + bodyContent.getString());
+        findCloudProvider().register(key, bodyContent.getString());
         return SKIP_BODY;
     }
 
