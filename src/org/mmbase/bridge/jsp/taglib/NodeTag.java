de792fb33a9c9766a41ff5f071af0aacb3466fa0
Pierre van Rooden <pvrooden@gmail.com>
2001-07-20 07:43:06 +0000

    pierre: Node now clears its reference after the end tag. Needed for using node in lists.

--- src/org/mmbase/bridge/jsp/taglib/NodeTag.java
+++ src/org/mmbase/bridge/jsp/taglib/NodeTag.java
@@ -22,15 +22,15 @@ import org.mmbase.bridge.Node;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 /**
-* NodeTag provides the fields of a node 
+* NodeTag provides the fields of a node
 *
 * @author Rob Vermeulen
 * @author Michiel Meeuwissen
 */
 public class NodeTag extends AbstractNodeProviderTag implements BodyTag {
-    
+
     private static Logger log = Logging.getLoggerInstance(NodeTag.class.getName());
-    
+
     private String number    = null;
     private String type      = null;
     private String element   = null;
@@ -38,13 +38,13 @@ public class NodeTag extends AbstractNodeProviderTag implements BodyTag {
     private Node   node      = null;
     private boolean unregistered = false;
 
-    
+
     /**
      * Release all allocated resources.
      */
-    public void release() {   
+    public void release() {
         log.debug("releasing");
-        super.release();       
+        super.release();
         number = null;
 	type = null ;
 	element = null;
@@ -52,7 +52,7 @@ public class NodeTag extends AbstractNodeProviderTag implements BodyTag {
         node = null;
 
     }
-    
+
     public void setNumber(String number) throws JspTagException {
         if (log.isDebugEnabled()) {
             log.debug("setting number to " + number);
@@ -60,7 +60,7 @@ public class NodeTag extends AbstractNodeProviderTag implements BodyTag {
         node = null;
         this.number = getAttributeValue(number);
     }
-    
+
 
     public void setReferid(String r) throws JspTagException{
         unregistered = false;
@@ -86,48 +86,48 @@ public class NodeTag extends AbstractNodeProviderTag implements BodyTag {
 
     /**
      * The element attribute is used to access elements of
-     * clusternodes. 
+     * clusternodes.
      */
     public void setElement(String e) {
         node = null;
         element = e;
     }
-    
-    
-    public int doStartTag() throws JspTagException{            
+
+
+    public int doStartTag() throws JspTagException{
 
         if (unregistered && id == null) id = referid; // must be reregistered as a node with same id as it had.
         if (node == null) {
             log.debug("node is null");
-            if (number != null) { 
+            if (number != null) {
                 // explicity indicated which node (by number or alias)
                 node = getCloudProviderVar().getNode(number);
                 if(node == null) {
                     throw new JspTagException("Cannot find Node with number " + number);
-                }         
-            } else { 
-                // get the node from a parent element.           
-                NodeProvider nodeProvider = (NodeProvider) findParentTag("org.mmbase.bridge.jsp.taglib.NodeProvider", null); 
+                }
+            } else {
+                // get the node from a parent element.
+                NodeProvider nodeProvider = (NodeProvider) findParentTag("org.mmbase.bridge.jsp.taglib.NodeProvider", null);
                 if (element != null) {
                     node = nodeProvider.getNodeVar().getNodeValue(element);
                 } else {
                     node = nodeProvider.getNodeVar();
                 }
-                
+
             }
         }
-        setNodeVar(node);        
-        //log.debug("found node " + node.getValue("gui()"));        
-        return EVAL_BODY_TAG; 
+        setNodeVar(node);
+        //log.debug("found node " + node.getValue("gui()"));
+        return EVAL_BODY_TAG;
     }
-    
-    public void doInitBody() throws JspTagException {       
+
+    public void doInitBody() throws JspTagException {
         log.debug("fillvars");
-        fillVars();    
-    } 
-    
-    
-    
+        fillVars();
+    }
+
+
+
     /**
     * this method writes the content of the body back to the jsp page
     **/
@@ -138,5 +138,10 @@ public class NodeTag extends AbstractNodeProviderTag implements BodyTag {
             throw new JspTagException(ioe.toString());
         }
         return SKIP_BODY;
+    }
+
+    public int doEndTag() throws JspTagException {
+        node = null;
+        return EVAL_PAGE;
     }
 }
