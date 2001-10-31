2eba44fda76d748838ae9da7e2ee4be29ad859b8
Michiel Meeuwissen <git@meeuw.org>
2001-10-31 21:43:47 +0000

    michiel: This one is needed for implementing Writer Tags

---
+++ src/org/mmbase/bridge/jsp/taglib/WriterHelper.java
@@ -0,0 +1,208 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.BodyContent;
+import java.io.IOException;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+
+/**
+ * Tags that are Writers can use the this class. It's a pitty that
+ * they can't extend, but that's life.
+ *
+ *
+ * @author Michiel Meeuwissen 
+ */
+
+public class WriterHelper {
+
+    private static Logger log = Logging.getLoggerInstance(WriterHelper.class.getName());
+
+    static final int TYPE_UNKNOWN = -10;
+    static final int TYPE_UNSET   = -1;
+    static final int TYPE_OBJECT  = 0;
+    static final int TYPE_LIST    = 1;
+    static final int TYPE_VECTOR  = 2;
+    static final int TYPE_INTEGER = 3;
+    static final int TYPE_STRING  = 4;
+    static final int TYPE_NODE    = 5;
+    static final int TYPE_BYTES   = 6;
+
+
+    static final int stringToType(String t) {
+        if ("String".equalsIgnoreCase(t)) {
+            return TYPE_STRING;
+        } else if ("Node".equalsIgnoreCase(t)) {
+            return TYPE_NODE;
+        } else if ("Integer".equalsIgnoreCase(t)) {
+            return TYPE_INTEGER;
+        } else if ("Vector".equalsIgnoreCase(t)) {
+            return TYPE_VECTOR;
+        } else if ("List".equalsIgnoreCase(t)) {
+            return TYPE_LIST;
+        } else if ("bytes".equalsIgnoreCase(t)) {
+            return TYPE_BYTES;
+        } else if ("Object".equalsIgnoreCase(t)) {
+            return TYPE_OBJECT;
+        } else {
+            return TYPE_UNKNOWN;
+        }
+    }
+
+    private   Object  value             = null;
+
+    private   String  jspvar           = null;
+    private   String  session          = null;
+    private   Boolean write            = null;
+    private   int     type             = TYPE_UNSET;
+    private   BodyContent  bodyContent;
+    
+    public void setWrite(Boolean w) {
+        write = w;
+    }
+    public boolean  isWrite() {
+        if (write == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("write is unset, using default with body == '" + bodyContent.getString() + "'");
+            }
+            return "".equals(bodyContent.getString());
+        } else {
+            return write.booleanValue();
+        }
+    }
+    
+    public void setSession(String s) {
+        session = s;
+    }
+    public String getSession() {
+        return session;
+    }
+
+    public void setJspvar(String j) {
+        jspvar = j;
+    }
+
+    public void setValue(Object v) {
+        value = v;
+    }
+    public void setBodyContent(BodyContent b) {
+        bodyContent = b;
+    }
+    public Object getValue() {
+        return value;
+    }
+
+    /**
+     * Don't forget to call 'setValue' first!
+     */
+    
+    protected void setJspVar(javax.servlet.jsp.PageContext pageContext) throws JspTagException {
+      
+        if (jspvar == null) return;
+        if (log.isDebugEnabled()) {
+            log.debug("Setting variable " + jspvar + " to " + value);
+        }
+        switch (type) {
+        case TYPE_LIST:
+            if (value instanceof java.util.List) {
+                pageContext.setAttribute(jspvar, value);
+                return;
+            }
+        case TYPE_VECTOR:
+            if (value == null) {
+                // if a vector is requested, but the value is not present,
+                // make a vector of size 0.
+                value = new java.util.Vector();
+            }
+            if (! (value instanceof java.util.Vector)) {
+                // if a vector is requested, but the value is not a vector,
+                if (! (value instanceof java.util.Collection)) {
+                    // not even a Collection!
+                    // make a vector of size 1.
+                    java.util.Vector v = new java.util.Vector();
+                    v.add(value);
+                    value = v;
+                } else {
+                    value = new java.util.Vector((java.util.Collection)value);
+                }
+            }                         
+            pageContext.setAttribute(jspvar, value);         
+            return;
+        }
+        if (value == null) {
+            pageContext.setAttribute(jspvar, null);
+            return;
+        } 
+
+        switch (type) {
+        case TYPE_INTEGER:
+            if (! (value instanceof Integer)) {
+                pageContext.setAttribute(jspvar, new Integer(value.toString()));
+                return;
+            } 
+            break;
+        case TYPE_STRING:
+            if (! (value instanceof String)) {
+                pageContext.setAttribute(jspvar, value.toString());
+                return;
+            } 
+            break;
+        case TYPE_NODE:
+            if (! (value instanceof org.mmbase.bridge.Node)) {
+                throw new JspTagException("Variable is not of type Node. Conversion is not yet supported by this Tag");
+            }
+            break;
+        }
+        pageContext.setAttribute(jspvar, value);
+
+    }
+
+
+    public void setType(String t) throws JspTagException {
+        type = stringToType(t);
+        if (type == TYPE_UNKNOWN) {
+            throw new JspTagException("Type " + t + " is not known");
+        }
+    }
+    public int getType() {
+        return type;
+    }
+
+    public String getBodyString() throws JspTagException {        
+        if (value instanceof byte[]) {         
+            // writing bytes to the page?? We write base64 encoded...
+            return org.mmbase.util.Encode.encode("BASE64", (byte[]) value); 
+        }
+        return value.toString();
+    }
+    
+    /**
+     * A basic afterbody for Writers.
+     *
+     * It decides if to write or not.
+     */
+    public int doAfterBody() throws JspTagException {
+        try {
+            String body = bodyContent.getString();
+            if (isWrite()) {
+                bodyContent.clearBody();
+                bodyContent.print(getBodyString() + body);
+            }
+            bodyContent.writeOut(bodyContent.getEnclosingWriter());
+        } catch (IOException ioe){
+            throw new JspTagException(ioe.toString());
+        }            
+        return javax.servlet.jsp.tagext.BodyTagSupport.SKIP_BODY;
+    }
+
+}
