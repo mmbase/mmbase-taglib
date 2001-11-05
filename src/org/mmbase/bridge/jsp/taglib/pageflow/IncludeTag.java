f7e47fde1f13dd9420549114408a702d68a6cae7
Michiel Meeuwissen <git@meeuw.org>
2001-11-05 13:49:08 +0000

    michiel: Works in Tomcat now, too

--- src/org/mmbase/bridge/jsp/taglib/pageflow/IncludeTag.java
+++ src/org/mmbase/bridge/jsp/taglib/pageflow/IncludeTag.java
@@ -8,6 +8,11 @@ See http://www.MMBase.org/license
 
 */
 package org.mmbase.bridge.jsp.taglib.pageflow;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import javax.servlet.jsp.JspTagException;
 
 /**
@@ -17,23 +22,45 @@ import javax.servlet.jsp.JspTagException;
  */
 public class IncludeTag extends UrlTag {
     public int doAfterBody() throws JspTagException {
-
+        
         if (page == null) {
             throw new JspTagException("Attribute 'page' was not specified");
         }
         try {
             bodyContent.clear(); // newlines and such must be removed            
-            pageContext.include(getUrl(false)); // false: don't write &amp; tags but real &.
+
+            String gotUrl = getUrl(false);// false: don't write &amp; tags but real &.
+            
+            // if not absolute, make it absolute:
+            // (how does one check something like that?)
+            String urlString;
+            if (gotUrl.indexOf(':') == -1) {
+                javax.servlet.http.HttpServletRequest request = (javax.servlet.http.HttpServletRequest)pageContext.getRequest();
+                urlString = 
+                    request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+                    + new java.io.File(request.getRequestURI()).getParent().toString() + "/" + gotUrl;
+            } else {
+                urlString = gotUrl;
+            }                
+                
+	    URL includeURL = new URL(urlString); 
+	    HttpURLConnection connection = (HttpURLConnection) includeURL.openConnection();
+	    connection.connect();
+	    
+	    BufferedReader in = new BufferedReader(new InputStreamReader
+                (connection.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null ) {
+		bodyContent.write(line);
+	    }
+	    
             if (getId() != null) {
-                getContextTag().register(getId(), bodyContent.getString());
+               getContextTag().register(getId(), bodyContent.getString());
             }
             bodyContent.writeOut(bodyContent.getEnclosingWriter());
         } catch (java.io.IOException e) {
             throw new JspTagException (e.toString());            
-        } catch (javax.servlet.ServletException e) {
-            throw new JspTagException (e.toString());        
-        }
-        
+        }        
         return SKIP_BODY;
     }
 
