619dc77aa2ecde16a5cd54aa73e5dab5a2112218
Michiel Meeuwissen <git@meeuw.org>
2002-07-24 15:28:19 +0000

    michiel: in some circonstances the name must be set to null again explicitely (tag reuse)

--- src/org/mmbase/bridge/jsp/taglib/security/SetContextTag.java
+++ src/org/mmbase/bridge/jsp/taglib/security/SetContextTag.java
@@ -11,6 +11,8 @@ package org.mmbase.bridge.jsp.taglib.security;
 import org.mmbase.bridge.jsp.taglib.NodeReferrerTag;
 import javax.servlet.jsp.JspTagException;
 
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
 
 /**
 * A very simple tag to change the context of the node
@@ -18,6 +20,8 @@ import javax.servlet.jsp.JspTagException;
 * @author Michiel Meeuwissen
 */
 public class SetContextTag extends NodeReferrerTag {
+
+    private static Logger log = Logging.getLoggerInstance(SetContextTag.class.getName());
     private String name = null;
 
     public void setName(String n) throws JspTagException {
@@ -28,7 +32,9 @@ public class SetContextTag extends NodeReferrerTag {
         if (name == null) {
             name = bodyContent.getString();
         }
+        if (log.isDebugEnabled()) log.debug("Setting context to " + name);
         getNode().setContext(name);
+        name = null;
         return EVAL_PAGE;
     }   
 
