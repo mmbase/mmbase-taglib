9d6ce2a8c001cb59de10bc6c4a06b587afd13eb5
Michiel Meeuwissen <git@meeuw.org>
2002-10-17 11:00:05 +0000

    michiel: another fix for tomcat 4.1

--- src/org/mmbase/bridge/jsp/taglib/pageflow/ParamTag.java
+++ src/org/mmbase/bridge/jsp/taglib/pageflow/ParamTag.java
@@ -11,6 +11,7 @@ package org.mmbase.bridge.jsp.taglib.pageflow;
 
 import org.mmbase.bridge.jsp.taglib.ContextReferrerTag;
 import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.JspException;
 
 
 /**
@@ -22,6 +23,8 @@ public class ParamTag extends ContextReferrerTag {
     
     private String name = null;
     private String value = null;
+    private UrlTag urlTag;
+    private boolean handled;
            
     public void setName(String n) throws JspTagException {
         name = getAttributeValue(n);
@@ -30,15 +33,23 @@ public class ParamTag extends ContextReferrerTag {
         value = getAttributeValue(v);
     }
 
-    public int doAfterBody() throws JspTagException {
+    public int doStartTag() throws JspException {
+        urlTag = (UrlTag) findParentTag("org.mmbase.bridge.jsp.taglib.pageflow.UrlTag", null);
+        handled = false;
+        return super.doStartTag();
+    }
 
-        // find the parent URL-tag
-        UrlTag urlTag = (UrlTag) findParentTag("org.mmbase.bridge.jsp.taglib.pageflow.UrlTag", null);
-        
+    public int doAfterBody() throws JspException {
         if (value == null) {
             // the value is the body context.      
             urlTag.addParameter(name, bodyContent.getString());
-        } else {
+            handled = true;
+        }
+        return super.doAfterBody();
+    }
+
+    public int doEndTag() throws JspException {
+        if (! handled && value != null) {
             urlTag.addParameter(name, value);
         }
         return SKIP_BODY;
