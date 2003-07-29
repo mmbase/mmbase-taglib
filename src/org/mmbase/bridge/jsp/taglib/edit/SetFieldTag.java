1e31f10eac50e3f6ebea375a3f650956aca10da0
Michiel Meeuwissen <git@meeuw.org>
2003-07-29 10:05:50 +0000

    added some code for setting of integers, wich are received as String. If it is e.g. 2.0 it will still work now

--- src/org/mmbase/bridge/jsp/taglib/edit/SetFieldTag.java
+++ src/org/mmbase/bridge/jsp/taglib/edit/SetFieldTag.java
@@ -20,11 +20,11 @@ import org.mmbase.util.logging.*;
  *
  * @author Michiel Meeuwissen
  * @author Jaco de Groot
- * @version $Id: SetFieldTag.java,v 1.21 2003-06-06 10:03:21 pierre Exp $ 
+ * @version $Id: SetFieldTag.java,v 1.22 2003-07-29 10:05:50 michiel Exp $ 
  */
 
 public class SetFieldTag extends FieldTag { // but it is not a writer
-    private static Logger log = Logging.getLoggerInstance(SetFieldTag.class.getName());
+    private static Logger log = Logging.getLoggerInstance(SetFieldTag.class);
 
     protected String convert (String s) throws JspTagException {
         return s;
@@ -49,14 +49,56 @@ public class SetFieldTag extends FieldTag { // but it is not a writer
         String body = "";
         if (bodyContent != null) body = bodyContent.getString();
         // Get the new value from the body.
-        if ((field != null) && (field.getType() == Field.TYPE_BYTE)) {
+
+        int type = field.getType();
+        if ((field != null) && (type == Field.TYPE_BYTE)) {
             // if the field type is a BYTE  thing, we expect a BASE64 encoded String...
             getNodeVar().setByteValue(fieldName, org.mmbase.util.Encode.decodeBytes("BASE64", body));
         } else {
             String newValue = convert(body);
-            getNodeVar().setValue(fieldName, newValue);
+            Object value;
+            // a bit of hackery to make it more likely that actually a right type is fed to the core.
+            // E.g. if you use ExprCalc to set an integer field, that would not work otherwise (because always double like '1.0')
+            switch(type) {
+            case Field.TYPE_NODE:
+            case Field.TYPE_INTEGER: {
+                try {
+                    value = new Integer(newValue);
+                } catch (NumberFormatException e) {
+                    try {
+                        log.debug("Values does not look like a integer, trying to round");
+                        value = new Integer(new Float(newValue).intValue());
+                    } catch (NumberFormatException e2) {
+                        // don't know any more, leave error-handling to core.
+                        value = newValue;
+                    }
+                }
+                break;
+            }
+            case Field.TYPE_LONG: {
+                try {
+                    value = new Long(newValue);
+                } catch (NumberFormatException e) {
+                    try {
+                        log.debug("Values does not look like a long, trying to round");
+                        value = new Long(new Float(newValue).longValue());
+                    } catch (NumberFormatException e2) {
+                        // don't know any more, leave error-handling to core.
+                        value = newValue;
+                    }
+                }
+                break;
+            }
+            default: // rest should go ok in core
+                value = newValue;
+            }
+
+            if (log.isDebugEnabled()) {
+                log.debug("Setting field " + fieldName + " to " + value);
+            }
+            getNodeVar().setValue(fieldName, value);
             if (getId() != null) {
-                getContextProvider().getContainer().register(getId(), newValue);
+                getContextProvider().getContainer().register(getId(), value);
             }
         }
         findNodeProvider().setModified();
