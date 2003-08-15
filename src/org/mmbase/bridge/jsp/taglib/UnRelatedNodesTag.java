0eb571ca115cb5b8e2873e5b2e369390eaaed851
Michiel Meeuwissen <git@meeuw.org>
2003-08-15 13:30:39 +0000

    create 'role' , 'searchdir' and 'excludeself' attributes

--- src/org/mmbase/bridge/jsp/taglib/UnRelatedNodesTag.java
+++ src/org/mmbase/bridge/jsp/taglib/UnRelatedNodesTag.java
@@ -8,7 +8,7 @@ See http://www.MMBase.org/license
 
 */
 package org.mmbase.bridge.jsp.taglib;
-
+import org.mmbase.bri dge.jsp.taglib.util.Attribute;
 
 import javax.servlet.jsp.JspTagException;
 
@@ -21,12 +21,29 @@ import org.mmbase.util.logging.Logging;
  * Like listnodes tag, but is is also a node-referrer, and substracts the related nodes of the referred node.
  *
  * @author Michiel Meeuwissen
- * @version $Id: UnRelatedNodesTag.java,v 1.1 2003-08-01 16:50:58 michiel Exp $ 
+ * @version $Id: UnRelatedNodesTag.java,v 1.2 2003-08-15 13:30:39 michiel Exp $ 
  * @since MMBase-1.7
  */
 
 public class UnRelatedNodesTag extends ListNodesTag {
     private static Logger log = Logging.getLoggerInstance(UnRelatedNodesTag.class);
+
+    protected Attribute role        = Attribute.NULL;
+    protected Attribute searchDir   = Attribute.NULL;
+    protected Attribute excludeSelf = Attribute.NULL;
+
+
+    public void setRole(String role) throws JspTagException {
+        this.role = getAttribute(role);
+    }
+
+    public void setSearchdir(String search) throws JspTagException {
+        searchDir = getAttribute(search);
+    }
+
+    public void setExcludeself(String e) throws JspTagException {
+        excludeSelf = getAttribute(e);
+    }
 
 
     protected NodeList getNodes() throws JspTagException {
@@ -37,7 +54,11 @@ public class UnRelatedNodesTag extends ListNodesTag {
         }
 
         NodeList nodes = super.getNodes();
-        NodeList relatedNodes = parentNode.getRelatedNodes(getNodeManager());
+        NodeList relatedNodes = parentNode.getRelatedNodes(getNodeManager(), (String) role.getValue(this), (String) searchDir.getValue(this));
+
+        if (excludeSelf.getBoolean(this, false)) {
+            nodes.remove(parentNode);
+        }
 
         nodes.removeAll(relatedNodes);
         return nodes;
