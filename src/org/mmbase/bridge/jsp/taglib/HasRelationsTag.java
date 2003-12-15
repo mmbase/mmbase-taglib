112caf13c824320e1f35fead69ffae6d8c2ba467
Michiel Meeuwissen <git@meeuw.org>
2003-12-15 18:19:53 +0000

    mm:hasrelations, which is an entrance to node.hasRelations. We used to do <mm:countrelatiosn><mm:isgreaterthan value=0, but that has two disadvantage: 1. it is slower (the related nodes are actually fetches, while in InsRel there is nice and fast impl for just that) 2. 'hasRelations' does not use security, so hasRealtions returs also true if there are relations which you may not see: this is more correct. .tld will follow as soon as i'm in sync

---
+++ src/org/mmbase/bridge/jsp/taglib/HasRelationsTag.java
@@ -0,0 +1,59 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib;
+import javax.servlet.jsp.*;
+
+import org.mmbase.bridge.jsp.taglib.util.Attribute;
+
+
+/**
+ * Whether current node has relations.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: HasRelationsTag.java,v 1.1 2003-12-15 18:19:53 michiel Exp $ 
+ * @since MMBase-1.7
+ */
+
+public class HasRelationsTag extends NodeReferrerTag implements Condition {
+
+    protected Attribute inverse = Attribute.NULL;
+
+    public void setInverse(String b) throws JspTagException {
+        inverse = getAttribute(b);
+    }
+    protected final boolean getInverse() throws JspTagException {
+        return inverse.getBoolean(this, false);
+    }
+
+    public int doStartTag() throws JspTagException {
+        if ((getNode().hasRelations()) != getInverse()) {
+            return EVAL_BODY_BUFFERED;
+        } else {
+            return SKIP_BODY;
+        }
+    }
+    public int doAfterBody() throws JspTagException {
+        try{
+            if(bodyContent != null) {
+                bodyContent.writeOut(bodyContent.getEnclosingWriter());
+            }
+            return SKIP_BODY;
+        } catch(java.io.IOException e){
+            throw new JspTagException("IO Error: " + e.getMessage());
+        }
+    }
+
+    public int doEndTag() throws JspTagException {     
+        return EVAL_PAGE;
+    }
+
+
+
+}
