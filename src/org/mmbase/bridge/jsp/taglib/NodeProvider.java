c02a6b547d6358db52a1ed6d729cab2dabb01c65
Michiel Meeuwissen <git@meeuw.org>
2003-12-18 11:54:37 +0000

    *** empty log message ***

--- src/org/mmbase/bridge/jsp/taglib/NodeProvider.java
+++ src/org/mmbase/bridge/jsp/taglib/NodeProvider.java
@@ -10,16 +10,18 @@ See http://www.MMBase.org/license
 package org.mmbase.bridge.jsp.taglib;
 import javax.servlet.jsp.JspTagException;
 
+import org.mmbase.bridge.jsp.taglib.containers.FunctionContainerOrNodeProvider;
+
 import org.mmbase.bridge.Node;
 /**
  * Interface designed to make it possible for child tags
  * to access a node defined in a tag
  *
  * @author Michiel Meeuwissen 
- * @version $Id: NodeProvider.java,v 1.5 2003-06-06 10:03:09 pierre Exp $ 
+ * @version $Id: NodeProvider.java,v 1.6 2003-12-18 11:54:37 michiel Exp $ 
  */
 
-public interface NodeProvider extends TagIdentifier {
+public interface NodeProvider extends TagIdentifier, FunctionContainerOrNodeProvider {
     /**
      * @return the node contained in the tag
      * NOTE: we have decided to call this methid getNodeVar because
