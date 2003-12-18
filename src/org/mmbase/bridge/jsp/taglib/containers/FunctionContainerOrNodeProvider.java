70f2061bdee57ebab70d7f5eda78cf007f7b5599
Michiel Meeuwissen <git@meeuw.org>
2003-12-18 11:59:33 +0000

    still missing

---
+++ src/org/mmbase/bridge/jsp/taglib/containers/FunctionContainerOrNodeProvider.java
@@ -0,0 +1,25 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib.containers;
+
+import java.util.List;
+
+import org.mmbase.bridge.jsp.taglib.*;
+import org.mmbase.util.functions.*;
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @since  MMBase-1.7
+ * @version $Id: FunctionContainerOrNodeProvider.java,v 1.1 2003-12-18 11:59:33 michiel Exp $
+ */
+public interface FunctionContainerOrNodeProvider {
+
+}
