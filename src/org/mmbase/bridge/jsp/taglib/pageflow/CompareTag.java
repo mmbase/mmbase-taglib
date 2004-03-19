81fdda6a89c56f909b7a41b20362afc85d25efc6
Michiel Meeuwissen <git@meeuw.org>
2004-03-19 13:21:42 +0000

    oops

--- src/org/mmbase/bridge/jsp/taglib/pageflow/CompareTag.java
+++ src/org/mmbase/bridge/jsp/taglib/pageflow/CompareTag.java
@@ -25,7 +25,7 @@ import java.math.BigDecimal;
  * variable equals a certain String value.
  *
  * @author Michiel Meeuwissen
- * @version $Id: CompareTag.java,v 1.30 2004-03-19 12:05:17 michiel Exp $
+ * @version $Id: CompareTag.java,v 1.31 2004-03-19 13:21:42 michiel Exp $
  */
 
 public class CompareTag extends PresentTag implements Condition, WriterReferrer {
@@ -129,7 +129,7 @@ public class CompareTag extends PresentTag implements Condition, WriterReferrer 
                 }
 
                 if (doCompare((Comparable)compare1, (Comparable)compare2) != getInverse() ) {
-                    return EVAL_BODY;
+                    return EVAL_BODY_BUFFERED;
                 }
                 
             }
@@ -144,11 +144,11 @@ public class CompareTag extends PresentTag implements Condition, WriterReferrer 
                         compare1n = new BigDecimal((String)compare1);
                     }
                     if (doCompare((Comparable)compare1n, (Comparable)compare2) != getInverse() ) {
-                        return EVAL_BODY; 
+                        return EVAL_BODY_BUFFERED; 
                     }
                 } else { // both compare1 and compare2 are not Number, simply compare then
                     if (doCompare((Comparable)compare1, (Comparable)compare2) != getInverse() ) {
-                        return EVAL_BODY; 
+                        return EVAL_BODY_BUFFERED; 
                     }
                 }
             }
