ec973ecea3ed9cb7cd2756fa76720aaf6b4e3c45
André van Toly <513194+elfuego@users.noreply.github.com>
2004-09-14 19:24:00 +0000

    added getGeneratingQuery()

--- src/org/mmbase/bridge/jsp/taglib/NodeProvider.java
+++ src/org/mmbase/bridge/jsp/taglib/NodeProvider.java
@@ -12,13 +12,14 @@ import javax.servlet.jsp.JspTagException;
 
 import org.mmbase.bridge.jsp.taglib.containers.FunctionContainerOrNodeProvider;
 
-import org.mmbase.bridge.Node;
+import org.mmbase.bridge.*;
 /**
  * Interface designed to make it possible for child tags
  * to access a node defined in a tag
  *
  * @author Michiel Meeuwissen 
- * @version $Id: NodeProvider.java,v 1.6 2003-12-18 11:54:37 michiel Exp $ 
+ * @version $Id: NodeProvider.java,v 1.7 2004-09-14 19:24:00 andre Exp $ 
+ * @todo EXPERIMENTAL
  */
 
 public interface NodeProvider extends TagIdentifier, FunctionContainerOrNodeProvider {
@@ -26,8 +27,12 @@ public interface NodeProvider extends TagIdentifier, FunctionContainerOrNodeProv
      * @return the node contained in the tag
      * NOTE: we have decided to call this methid getNodeVar because
      * we use tag attributes with name "node" and type String 
+     *
+     * Experimental: getGeneratingQuery()
      **/
     public Node getNodeVar() throws JspTagException;	
     public void setModified();
     public void setJspvar(String jv);
+    
+    public Query getGeneratingQuery() throws JspTagException;		// experimental
 }
