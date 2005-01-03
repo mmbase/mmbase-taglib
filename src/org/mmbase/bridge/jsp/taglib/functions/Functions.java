4fbd3cdf3a94d2ef6739b873d18554bef186be09
Michiel Meeuwissen <git@meeuw.org>
2005-01-03 21:35:27 +0000

    implementaiton of mm:contains

---
+++ src/org/mmbase/bridge/jsp/taglib/functions/Functions.java
@@ -0,0 +1,54 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib.functions;
+
+import java.util.*;
+import javax.servlet.jsp.JspTagException;
+import org.mmbase.bridge.*;
+import org.mmbase.util.Casting;
+
+/**
+ * Functions for EL variables.
+ * Like this:
+
+<mm:import id="nodelist" vartype="list">1,2,123</mm:import>
+<mm:cloud>
+  <mm:node number="124" id="node" />
+  <c:choose>
+    <c:when test="${mm:contains(nodelist, node)}">
+      YES!
+    </c:when>
+    <c:otherwise>
+      NO!
+    </c:otherwise>
+  </c:choose>
+</mm:cloud>
+ *
+ * @author  Michiel Meeuwissen
+ * @since   MMBase-1.8
+ * @version $Id: Functions.java,v 1.1 2005-01-03 21:35:27 michiel Exp $
+ */
+public class Functions {
+
+    /**
+     * MMBase specific 'contains' (for Collections). For strings use fn:contains.
+     */
+    public static boolean contains(Collection col, Object obj) {
+        if (obj instanceof Node) {
+            if (col instanceof NodeList) {
+                if (col.contains(obj)) return true;
+            } else {
+                obj = new Integer(((Node) obj).getNumber());
+            }
+        }
+        if (col.contains(obj)) return true;
+        return col.contains(Casting.toString(obj));
+    }
+}
