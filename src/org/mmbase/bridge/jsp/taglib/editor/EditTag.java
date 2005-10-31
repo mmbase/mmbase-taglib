2c2f8f816cd004ca3c9a482e44d61c6885c904fb
André van Toly <513194+elfuego@users.noreply.github.com>
2005-10-31 20:57:01 +0000

    Adding the EditTag to the repository.

---
+++ src/org/mmbase/bridge/jsp/taglib/editor/EditTag.java
@@ -0,0 +1,185 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib.editor;
+
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.*;
+import java.io.IOException;
+import java.util.*;
+
+import org.mmbase.bridge.*;
+import org.mmbase.storage.search.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+import org.mmbase.bridge.jsp.taglib.util.Attribute;
+import org.mmbase.bridge.jsp.taglib.ContextReferrerTag;
+import org.mmbase.bridge.jsp.taglib.ParamHandler;
+import org.mmbase.util.Entry;
+
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.mmbase.util.XMLBasicReader;
+
+/**
+ * The EditTag collects the nodenrs, fields and queries of the FieldTags in its body.
+ * FieldTags register these with the EditTag. You can pass these values to an 
+ * editor by creating an url for example like YAMMEditor does which gives access to
+ * the editor yammeditor.jsp.<br />
+ * Extend Editor to create your own and edit the edittagtypes.xml resources file
+ * to let the EditTag know about it.
+ *
+ * @author Andr&eacute; van Toly
+ * @version $Id: EditTag.java,v 1.1 2005-10-31 20:57:01 andre Exp $
+ * @see org.mmbase.bridge.jsp.taglib.editor.Editor
+ * @see org.mmbase.bridge.jsp.taglib.editor.EditTagYAMMe
+ */
+public class EditTag extends ContextReferrerTag implements ParamHandler {
+
+    private static final Logger log = Logging.getLoggerInstance(EditTag.class);
+    private Attribute type = Attribute.NULL;
+    
+    private static Query query;
+    private static int nodenr;
+    private static String fieldName;
+
+    private static ArrayList queryList = new ArrayList();		// query List
+    private static ArrayList nodenrList = new ArrayList();      // nodenr List
+    private static ArrayList fieldList = new ArrayList();       // fieldname List
+
+    private static final Map edittagTypes = new HashMap();	// edittagtype -> class
+    protected List parameters;
+    
+    private Editor yaeditor = null;		// should do all the work
+    
+    /**
+     * The type of editor, see resources/edittagtypes.xml for different types or
+     * add your own editor.
+     *
+     * @param t     String with editor type.
+     */ 
+    public void setType(String t) throws JspTagException {
+        type = getAttribute(t);
+    }
+
+    /**
+     * The type of editor, see resources/edittagtypes.xml for different types or
+     * add your own editor.
+     *
+     * @return String stating the type of editor the tag refers to.
+     */ 
+    public String getType() throws JspTagException {
+        if (type == Attribute.NULL) {
+            return "edittag";			// the default
+        } else {
+            return type.getString(this);
+        }
+    }
+    
+    /**
+     * Parameters of this tag. The tag's type is defined in its attribute, but you
+     * can give it as much parameters as you like to controll your implementations
+     * behaviour.
+     *
+     * @param key     String with a key
+     * @param value   Object with a value
+     */ 
+    public void addParameter(String key, Object value) throws JspTagException {
+        log.debug("adding parameter " + key + "/" + value);
+        parameters.add(new Entry(key, value));
+    }
+    
+    /*
+     * Start the EditTag, put the implementations found in its resources in a Map, 
+     * consult the attribute type which implementation to use and instantiate it.
+     *
+     *
+    */    
+    public int doStartTag() throws JspTagException {
+        log.debug("doStartTag of EditTag");
+        parameters = new ArrayList();
+        
+        InputSource ettypes = new InputSource(EditTag.class.getResourceAsStream("edittagtypes.xml"));
+        XMLBasicReader reader  = new XMLBasicReader(ettypes, EditTag.class);
+        
+        Element edittypesElement = reader.getElementByPath("edittagtypes");
+        Iterator i = reader.getChildElements(edittypesElement, "editor");
+        while (i.hasNext()) {
+            Element element = (Element) i.next();
+            String type = element.getAttribute("type");
+            String claz = reader.getElementValue(reader.getElementByPath(element, "editor.class"));
+            if (!claz.equals("")) {
+	            edittagTypes.put(type, claz);
+                log.debug("Found editor type: '" + type + "' with class: '" + claz + "'");
+	        } 
+        }
+        
+        String classname = (String) edittagTypes.get(getType());
+        log.debug("Using editor: " + classname);
+        Class c = null;
+        try {
+        	c = Class.forName(classname);
+        	yaeditor = (Editor) c.newInstance();
+        } catch (ClassNotFoundException cnfe) {
+        	log.error("Class '" + classname + "' not found: " + cnfe);
+        } catch (InstantiationException ie) {
+        	log.error("Unable to instantiate class '" + classname + "': " + ie);
+        } catch (IllegalAccessException iae) {
+        	log.error("IllegalAccessException instantiating class " + classname + "': " + iae);	
+        }
+                
+        return EVAL_BODY_INCLUDE;
+    }
+    
+    /*
+     * Pass all gathered information to the implementing editor, get the
+     * the result back and write it to the page.
+     *
+    */
+    public int doEndTag() throws JspTagException {
+		String editorstr = "";
+		
+		yaeditor.setParameters(parameters);
+		
+		yaeditor.setQueryList(queryList);
+		yaeditor.setNodenrList(nodenrList);
+		yaeditor.setFieldList(fieldList);
+		
+		yaeditor.registerFields(queryList, nodenrList, fieldList);
+		
+		// resulting String
+		editorstr = yaeditor.getEditorHTML();
+		
+		helper.setValue(editorstr);
+		helper.useEscaper(false);
+		
+		parameters = null;
+		queryList = nodenrList = fieldList = null;
+        log.debug("end of doEndTag of EditTag");
+		helper.doEndTag();
+		return super.doEndTag();
+    }
+    
+    /**
+     * Here is were the FieldTag registers its fields and some associated 
+     * and maybe usefull information with the EditTag.
+     *
+     * @param query     SearchQuery object that delivered the field
+     * @param nodenr    int with the number of the node the field belongs to
+     * @param field     String with the fieldname
+     */ 
+    public void registerField(Query query, int nodenr, String fieldName) {
+        queryList.add(query);
+        nodenrList.add(String.valueOf(nodenr));
+        fieldList.add(fieldName);
+		
+		log.debug("ET registers nodenr '" + nodenr + "' and fieldName '" + fieldName + "'");
+    }   
+}
---
+++ src/org/mmbase/bridge/jsp/taglib/editor/YAMMEditor.java
@@ -0,0 +1,305 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib.editor;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.mmbase.bridge.*;
+import org.mmbase.storage.search.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+import org.mmbase.util.Entry;
+
+/**
+ * This is an example implementation of the EditTag. It extends the implementation
+ * class Editor of EditTag. To create your own editor with the edittag you could
+ * or rather should also extend Editor.<br />
+ * EditTagYAMMe works together with the editor yammeditor.jsp. It creates an URL 
+ * that looks something like this:<br />
+ * yammeditor.jsp?nrs=76&fields=76_number;76_title;76_subtitle;76_intro;80_gui();
+ *
+ * @author Andr&eacute; van Toly
+ * @version $Id: YAMMEditor.java,v 1.1 2005-10-31 20:57:01 andre Exp $
+ * @see org.mmbase.bridge.jsp.taglib.editor.EditTag
+ * @see org.mmbase.bridge.jsp.taglib.editor.Editor
+ */
+
+public class YAMMEditor extends Editor {
+
+    private static final Logger log = Logging.getLoggerInstance(YAMMEditor.class);
+
+    private static Query query;
+    private static int nodenr;
+    private static String fieldName;
+    
+    private static ArrayList queryList = new ArrayList();
+    private static ArrayList nodenrList = new ArrayList();
+    private static ArrayList fldList = new ArrayList();
+    
+    protected List parameters;
+
+    private static String editor;
+    private static String icon;
+    private static ArrayList startList = new ArrayList();       // startnodes: 346
+    private static ArrayList pathList  = new ArrayList();       // paths: 346_news,posrel,urls
+    private static ArrayList nodeList  = new ArrayList();       // nodes: 602 (should be 346.602)
+    private static ArrayList fieldList = new ArrayList();       // fields: 602_news.title    
+    // Map to accommadate the fields and their startnodes
+    Map fld2snMap = new HashMap();
+    
+	/**
+	 * @param parameters Parameters of the edittag.
+	 */
+	public void setParameters(List params) {
+	   this.parameters = params;
+       log.debug("parameters: " + parameters);
+	}
+	
+    public void setQueryList(ArrayList qlist) {
+    	this.queryList = qlist;
+    }
+    public void setNodenrList(ArrayList nrlist) {
+    	this.nodenrList = nrlist;
+    }
+    public void setFieldList(ArrayList flist) {
+    	this.fldList = flist;
+    }
+
+	/**
+     * Create a string containing all the needed html to find the editor. Or rather
+     * it calls one of the older methods i wrote, that was easier then rewriting
+     * the old method.
+     *
+     * @return	A String with a link and an icon to access yammeditor.jsp
+     */
+    public String getEditorHTML() {
+    	String html = "Sorry. You should see an icon and a link to yammeditor here.";
+    	
+    	// Parameters 
+    	String url = "";
+    	String icon = "";
+    	
+    	Iterator pi = parameters.iterator();
+    	while (pi.hasNext()) {
+    		Entry entry = (Entry) pi.next();
+    		String key = (String) entry.getKey();
+    		
+    		if (key.equals("url")) url = (String) entry.getValue();
+    		if (key.equals("icon")) icon = (String) entry.getValue();
+    	} 
+    	
+    	html = makeHTML(url, icon);
+    	// log.debug("returning: " + html);
+    	return html;
+    }
+    
+    /**
+     * Values passed by the EditTag from the FieldTags.
+     *
+     * @param queryList     ArrayList with SearchQuery objects from fields
+     * @param nodenrList    ArrayList with nodenumbers
+     * @param fieldList     ArrayList with fieldnames 
+     */ 
+    public void registerFields(ArrayList queryList, ArrayList nodenrList, ArrayList fieldList) {
+    	log.debug("processing fields");
+    	for (int i = 0; i < nodenrList.size(); i++) {
+    		String fldName = (String) fieldList.get(i);
+    		log.debug("processing field '" + fldName);
+    		Query query = (Query) queryList.get(i);
+    		int nodenr = Integer.parseInt( (String) nodenrList.get(i) );
+    		
+    		processField(query, nodenr, fldName);
+    	}
+    }
+
+    /**
+     * Here is were the FieldTag registers its fields and some associated 
+     * and maybe usefull information with EditTagYAMMe.
+     *
+     * @param query     SearchQuery that delivered the field
+     * @param nodenr    Nodenumber of the node the field belongs to
+     * @param field     Name of the field
+     */ 
+    public void processField(Query query, int nodenr, String field) {
+        this.query = query;
+        this.nodenr = nodenr;
+        this.fieldName = field;		// field
+        
+        //log.debug("Query : " + query);
+        
+        String path = getPathFromQuery(query);
+        if (path != null && !path.equals("") && !pathList.contains(path)) {
+            pathList.add(path);
+            log.debug("Added path : " + path);
+        }
+        
+        ArrayList nl = getNodesFromQuery(query, nodenr);
+        Iterator e = nl.iterator();			// iterate over the startnodes
+        while (e.hasNext()) {
+            String nr = (String)e.next();
+            boolean startnode = false;
+
+            
+            /* fills fld2snMap (only used to keep track of startnodes, 
+            it contains nodenr's
+            when a nr is found in this map ...
+            */
+            
+            if (!fld2snMap.containsValue(nr) ) {
+                fld2snMap.put(String.valueOf(nodenr), nr);
+                log.debug("Added nodenr : " + nodenr + " sn : " + nr);
+            } else if (fld2snMap.isEmpty()) {
+                fld2snMap.put(String.valueOf(nodenr), nr);
+            	startnode = true;
+                log.debug("Added nodenr (startnode): " + nodenr + " sn : " + nr);
+            } else {					// a node is a startnode when there was
+           		startnode = true;		//   no previous field with this nodenr as startnodenr
+            }
+            
+            // fill startList (startnodes)
+            if (!startList.contains(nr) && startnode) {			// 507 (= just startnodenr)
+                startList.add(nr);
+                log.debug("Added startnode : " + nr);
+            }
+            
+            // fill nodeList (just the nodes in a page)
+            String str = nr + "_" + String.valueOf(nodenr);		// 507_234 (= startnodenr_nodenr)
+			if (!nodeList.contains(str)) {
+				nodeList.add(str);
+				log.debug("Added nodenr : " + str);
+			}
+			
+			// fill fieldList (all the used fields in a page)	// 507_title (= startnodenr_fieldname)
+            String fieldstr = nr + "_" + fieldName;
+            if (!fieldList.contains(fieldstr)) {
+                fieldList.add(fieldstr);
+                log.debug("Added field : " + fieldstr);
+            }
+        }
+
+    }
+        
+    public ArrayList getNodesFromQuery(Query query, int nr) {
+        ArrayList nl = new ArrayList();
+        java.util.List steps = query.getSteps();
+        String number = String.valueOf(nr);
+        
+        if (steps.size() == 1) {    // why ?
+            nl.add(number);
+            log.debug("1. added nr to list of all the nodes in query: " + number);
+        } 
+        
+        Iterator si = steps.iterator();
+        while (si.hasNext()) {
+            Step step = (Step) si.next();
+            
+            // Get the nodes from this step 
+            //   (haalt alle nodes uit step, itereert erover en stopt ze in nl )
+            SortedSet nodeSet = step.getNodes();
+            for (Iterator nsi = nodeSet.iterator(); nsi.hasNext();) {
+                Integer n = (Integer)nsi.next();
+                number = String.valueOf(n);
+                
+                if (!nl.contains(number)) {
+                    nl.add(number);
+            		log.debug("2. added nr to list of all the nodes in query: " + number);
+                }
+            }
+            
+        }
+        return nl;
+    }
+    
+    /**
+    * Just get the path from this query
+    *
+    * @param query  The query
+    * @return       A path like 345_news,posrel,urls which is the nodenumber of
+    *               the node this field belongs to and the path that leads to it.
+    */  
+    public String getPathFromQuery(Query query) {
+        String path = null;     
+        
+        java.util.List steps = query.getSteps();
+        if (steps.size() > 1) {
+            Iterator si = steps.iterator();
+            while (si.hasNext()) {
+                Step step = (Step) si.next();
+                            
+                String nodenrs = "";
+                SortedSet nodeSet = step.getNodes();	// Get the (start?)nodes from this step
+                for (Iterator nsi = nodeSet.iterator(); nsi.hasNext();) {
+                    Integer number = (Integer)nsi.next();
+                    if (nodenrs.equals("")) {
+                        nodenrs = String.valueOf(number);
+                    } else {
+                        nodenrs = nodenrs + "," + String.valueOf(number);
+                    }
+                    
+                }
+                
+                // path: Get one nodetype at the time (the steps)
+                if (step.getAlias() != null) {
+                    if (path == null || path.equals("")) {
+                        path = nodenrs + "_" + step.getAlias();
+                    } else {
+                        path = path + "," + step.getAlias();
+                    }
+                }
+            }
+        }
+        return path;
+    }
+    
+    /**
+    * Creates a ; seperated string for the url with paths, fields or startnodes.
+    * 
+    * @param 	ArrayList	One of the ArrayLists
+    * @return	A ; seperated string with the elements from the ArrayList
+    *
+    */
+    public static String makeList4Url(ArrayList al) {
+        String str = "";
+        if (al.size() > 0) {
+            Iterator e = al.iterator();
+            while(e.hasNext()) {
+                if (str.equals("")) { 
+                    str = (String)e.next();
+                } else { 
+                    str = str + ";" + e.next();
+                }
+            }
+        }
+        return str;
+    }
+    
+    /**
+    * Creates a string with the link (and icon) to the editor
+    *
+    * @param editor     An url to an editor
+    * @param icon       An url to a graphic file
+    * @return           An HTML string with a link suitable for the editor yammeditor.jsp
+    * 
+    */
+    public static String makeHTML(String editor, String icon) {
+        String url = editor + "?nrs=" + makeList4Url(startList) + 
+            "&amp;fields=" + makeList4Url(fieldList) +
+            "&amp;paths=" + makeList4Url(pathList) +
+            "&amp;nodes=" + makeList4Url(nodeList);
+        String html = "<div class=\"et\"><a title=\"click to edit\" href=\"" + url + "\" onclick=\"window.open(this.href); return false;\">edit</a></div>";
+        if (!icon.equals("")) {
+            html = "<div class=\"et\"><a title=\"click me to edit\" href=\"" + url + "\" onclick=\"window.open(this.href); return false;\"><img src=\"" + icon + "\" alt=\"edit\"></a></div>";
+        }
+        return html;
+    }
+    
+}
