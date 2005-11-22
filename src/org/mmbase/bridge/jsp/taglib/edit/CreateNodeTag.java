e0aab39892d3366ccd0c0eb8c09c92a613bad0ac
Michiel Meeuwissen <git@meeuw.org>
2005-11-22 14:39:45 +0000

    cleaned up a bit

--- src/org/mmbase/bridge/jsp/taglib/edit/CreateNodeTag.java
+++ src/org/mmbase/bridge/jsp/taglib/edit/CreateNodeTag.java
@@ -21,7 +21,7 @@ import org.mmbase.util.logging.*;
  * can use `setField's in the body.
  *
  * @author Michiel Meeuwissen
- * @version $Id: CreateNodeTag.java,v 1.20 2004-07-26 20:18:02 nico Exp $
+ * @version $Id: CreateNodeTag.java,v 1.21 2005-11-22 14:39:45 michiel Exp $
  */
 
 public class CreateNodeTag extends NodeTag {
@@ -29,16 +29,10 @@ public class CreateNodeTag extends NodeTag {
     private static final Logger log = Logging.getLoggerInstance(CreateNodeTag.class);
 
     private Attribute nodeManager = Attribute.NULL;
-    private Attribute makeUniques = Attribute.NULL;
 
     public void setType(String t) throws JspTagException {
         nodeManager = getAttribute(t);
     }
-
-    public void setMakeuniques(String u) throws JspTagException {
-        makeUniques = getAttribute(u);
-    }
-
 
     public int doStartTag() throws JspTagException{
         Cloud cloud = getCloudVar();
@@ -53,9 +47,6 @@ public class CreateNodeTag extends NodeTag {
 
         setNodeVar(node);
         setModified();
-        if (log.isDebugEnabled()) {
-            log.debug("created node " + node.getNumber() + ": " + node.getValue("gui()"));
-        }
         fillVars();
         return EVAL_BODY_BUFFERED;
     }
