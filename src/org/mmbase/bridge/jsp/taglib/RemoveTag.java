e6f2afd2c80abb7dcf460d070cfa686d464310eb
Michiel Meeuwissen <git@meeuw.org>
2006-03-29 00:43:29 +0000

    granted #6746

--- src/org/mmbase/bridge/jsp/taglib/RemoveTag.java
+++ src/org/mmbase/bridge/jsp/taglib/RemoveTag.java
@@ -10,19 +10,22 @@ See http://www.MMBase.org/license
 package org.mmbase.bridge.jsp.taglib;
 import javax.servlet.jsp.JspTagException;
 import java.util.Collection;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.http.*;
 import org.mmbase.bridge.jsp.taglib.functions.Functions;
-import org.mmbase.bridge.jsp.taglib.util.Attribute;
+import org.mmbase.bridge.jsp.taglib.util.*;
 
 /**
  * Remove an object from the Context.
- * 
+ *
  * @author Michiel Meeuwissen
- * @version $Id: RemoveTag.java,v 1.10 2005-01-03 22:10:45 michiel Exp $ 
+ * @version $Id: RemoveTag.java,v 1.11 2006-03-29 00:43:29 michiel Exp $
  */
 
 public class RemoveTag extends ContextReferrerTag {
 
     private Attribute value = Attribute.NULL;
+    private Attribute from = Attribute.NULL;
 
     /**
      * @since MMBase-1.8
@@ -30,15 +33,90 @@ public class RemoveTag extends ContextReferrerTag {
     public void setValue(String v) throws JspTagException {
         value = getAttribute(v);
     }
-    
-    public int doEndTag() throws JspTagException {
+
+    /**
+     * @since MMBase-1.8
+     */
+    public void setFrom(String f) throws JspTagException {
+        from = getAttribute(f);
+    }
+
+    /**
+     * @since MMBase-1.8
+     */
+    protected void remove(ContextContainer cc, String referid) throws JspTagException {
         if (value.getString(this).equals("")) {
-            getContextProvider().getContextContainer().unRegister(getReferid());
+            cc.unRegister(referid);
         } else {
-            Collection col = (Collection) getContextProvider().getContextContainer().get(getReferid());
+            Collection col = (Collection) cc.get(referid);
             Functions.remove(col, value.getValue(this));
         }
+    }
+
+    public int doEndTag() throws JspTagException {
+        String fromString = from.getString(this);
+        if (! "".equals(fromString)) {
+            boolean useCollection = ! value.getString(this).equals("");
+            Collection col = null;
+            int from = ContextContainer.stringToLocation(fromString);
+            switch(from) {
+            case ContextContainer.LOCATION_PARENT:
+                if (useCollection) {
+                    col = (Collection) getContextProvider().getContextContainer().getParent().get(getReferid());
+                    break;
+                } else {
+                    remove(getContextProvider().getContextContainer().getParent(), getReferid());
+                    return EVAL_PAGE;
+                }
+            case ContextContainer.LOCATION_PAGE:
+                if (useCollection) {
+                    col = (Collection) pageContext.getAttribute(getReferid());
+                    break;
+                } else {
+                    pageContext.removeAttribute(getReferid());
+                    return EVAL_PAGE;
+                }
+            case ContextContainer.LOCATION_REQUEST:
+                if (useCollection) {
+                    col = (Collection) pageContext.getAttribute(getReferid(), PageContext.REQUEST_SCOPE);
+                    break;
+                } else {
+                    pageContext.removeAttribute(getReferid(), PageContext.REQUEST_SCOPE);
+                    return EVAL_PAGE;
+                }
+            case ContextContainer.LOCATION_APPLICATION:
+                if (useCollection) {
+                    col = (Collection) pageContext.getAttribute(getReferid(), PageContext.APPLICATION_SCOPE);
+                    break;
+                } else {
+                    pageContext.removeAttribute(getReferid(), PageContext.APPLICATION_SCOPE);
+                    return EVAL_PAGE;
+                }
+            case ContextContainer.LOCATION_SESSION:
+                HttpSession session = ((HttpServletRequest) pageContext.getRequest()).getSession(false);
+                if (session != null) {
+                    if (useCollection) {
+                        col = (Collection) session.getAttribute(getReferid());
+                        break;
+                    } else {
+                        session.removeAttribute(getReferid());
+                    }
+                }
+                return EVAL_PAGE;
+            case ContextContainer.LOCATION_THIS:
+                break;
+            default: {
+                throw new JspTagException("Unknown value for 'from' attribute " + fromString);
+            }
+            }
+            if (col != null) {
+                Functions.remove(col, value.getValue(this));
+                return EVAL_PAGE;
+            }
+        }
+
+        remove(getContextProvider().getContextContainer(), getReferid());
         return EVAL_PAGE;
     }
-    
+
 }
