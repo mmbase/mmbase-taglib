bb9276cdd9e5d7ccd42b25ccc101513eae63428a
Michiel Meeuwissen <git@meeuw.org>
2006-04-29 13:27:14 +0000

    resolved the todo, can not change and upload images in mm:form too

--- src/org/mmbase/bridge/jsp/taglib/typehandler/BinaryHandler.java
+++ src/org/mmbase/bridge/jsp/taglib/typehandler/BinaryHandler.java
@@ -15,11 +15,16 @@ import javax.servlet.jsp.JspTagException;
 import java.util.*;
 import org.mmbase.bridge.*;
 import org.mmbase.bridge.jsp.taglib.*;
-import org.mmbase.bridge.jsp.taglib.util.ContextContainer;
+import org.mmbase.bridge.jsp.taglib.edit.FormTag;
+import org.mmbase.bridge.jsp.taglib.util.*;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 import org.mmbase.util.functions.*;
+import org.mmbase.util.*;
+import org.mmbase.util.transformers.Xml;
+import org.mmbase.datatypes.*;
 import javax.servlet.jsp.PageContext;
+import org.apache.commons.fileupload.FileItem;
 
 /**
  * @javadoc
@@ -27,7 +32,7 @@ import javax.servlet.jsp.PageContext;
  * @author Gerard van de Looi
  * @author Michiel Meeuwissen
  * @since  MMBase-1.8 (was named ByteHandler previously)
- * @version $Id: BinaryHandler.java,v 1.2 2006-04-27 17:37:25 michiel Exp $
+ * @version $Id: BinaryHandler.java,v 1.3 2006-04-29 13:27:14 michiel Exp $
  */
 
 public class BinaryHandler extends AbstractTypeHandler {
@@ -48,7 +53,7 @@ public class BinaryHandler extends AbstractTypeHandler {
         if (node != null) {
             Function gui = node.getFunction("gui");
             Parameters args = gui.createParameters();
-            args.set("field", ""); // lot of function implementations would not stand 'null' as field name value
+            args.set("field", field.getName());
             args.set(Parameter.LANGUAGE, tag.getLocale().getLanguage());
             args.set("session",  tag.getSessionName());
             PageContext pc = tag.getContextTag().getPageContext();
@@ -64,32 +69,81 @@ public class BinaryHandler extends AbstractTypeHandler {
 
     }
 
+
+    /**
+     * Returns the field value as specified by the client's post.
+     */
+    protected Object getFieldValue(Field field) throws JspTagException {
+        if (MultiPart.isMultipart(tag.getPageContext())) {
+            ContextContainer cc = tag.getContextProvider().getContextContainer();
+            ContextTag ct = tag.getContextTag();
+            FileItem bytes = ct.getFileItem(prefix(field.getName()));
+            return bytes;
+        } else {
+            return null;
+        }
+    }
+
     public String checkHtmlInput(Node node, Field field, boolean errors) throws JspTagException {
-        // XXXX TODO
-        return "";
+        Object fieldValue = getFieldValue(field);
+
+        if (fieldValue != null) {
+            DataType dt = field.getDataType();
+            Collection col = dt.validate(fieldValue, node, field);
+            if (col.size() == 0) {
+                // do actually set the field, because some datatypes need cross-field checking
+                // also in an mm:form, you can simply commit.
+                if (node != null && ! field.isReadOnly()) {
+                    setValue(node, field, (FileItem) fieldValue);
+                }
+                if (errors) {
+                    return "<div id=\"" + prefixError(field.getName()) + "\" class=\"mm_check_noerror\"> </div>";
+                } else {
+                    return "";
+                }
+            } else {
+                FormTag form =  (FormTag) tag.findParentTag(FormTag.class, null, false);
+                if (form != null) {
+                    form.setValid(false);
+                }
+                if (errors) {
+                    StringBuffer show = new StringBuffer("<div id=\"");
+                    show.append(prefixError(field.getName()));
+                    show.append("\" class=\"mm_check_error\">");
+                    Locale locale =  tag.getLocale();
+                    Iterator i = col.iterator();
+                    while (i.hasNext()) {
+                        LocalizedString error = (LocalizedString) i.next();
+                        show.append("<span>");
+                        Xml.XMLEscape(error.get(locale), show);
+                        show.append("</span>");
+                    }
+                    show.append("</div>");
+                    return show.toString();
+                } else {
+                    return "";
+                }
+            }
+        } else {
+            return "";
+        }
     }
 
     /**
-     * @see TypeHandler#useHtmlInput(Node, Field)
+     * Sets the binary value. Also tries to set some fields which are generaly associated with the binary.
+     * This could actually be moved to commit-processors of those fields.
      */
-    public boolean useHtmlInput(Node node, Field field) throws JspTagException {
-        String fieldName = field.getName();
-        ContextTag ct = tag.getContextTag();
-        ContextContainer cc = tag.getContextProvider().getContextContainer();
-        org.apache.commons.fileupload.FileItem bytes = ct.getFileItem(prefix(fieldName));
-
-        if (bytes == null){
-            throw new BridgeException("getBytes(" + prefix(fieldName) + ") returned null (node= " +  node.getNumber() +") field=(" + field + ") (Was your form  enctype='multipart/form-data' ?");
-        }
+    protected void setValue(Node node, Field field, FileItem bytes) throws JspTagException {
         if (bytes.getSize() > 0) {
             String fileName = bytes.getName();
             String fileType = bytes.getContentType();
 
             try {
-                node.setInputStreamValue(fieldName, bytes.getInputStream(), bytes.getSize());
+                node.setInputStreamValue(field.getName(), bytes.getInputStream(), bytes.getSize());
             } catch (java.io.IOException ioe) {
                 throw new TaglibException(ioe);
             }
+            ContextContainer cc = tag.getContextProvider().getContextContainer();
             log.debug("Filename : " + fileName);
             NodeManager nm = node.getNodeManager();
             if (nm.hasField("mimetype") && (fileType != null) && (! fileType.equals("")) &&
@@ -112,7 +166,19 @@ public class BinaryHandler extends AbstractTypeHandler {
                 ) {
                 node.setLongValue("filesize", bytes.getSize());
             }
+        } 
+    }
+
+    /**
+     * @see TypeHandler#useHtmlInput(Node, Field)
+     */
+    public boolean useHtmlInput(Node node, Field field) throws JspTagException {
+        FileItem bytes = (FileItem) getFieldValue(field);
+        if (bytes == null){
+            throw new BridgeException("getBytes(" + prefix(field.getName()) + ") returned null (node= " +  node.getNumber() +") field=(" + field + ") (Was your form  enctype='multipart/form-data' ?");
         }
+        setValue(node, field, bytes);
+
 
         return true;
     }
