0977774091b009cefa27f629993fa6d5b3b9d2e3
sdeboer <sdeboer@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-08-03 12:08:02 +0000

    implemented notfound parameter MMB-1492

--- src/org/mmbase/bridge/jsp/taglib/functions/NodeFunctionTag.java
+++ src/org/mmbase/bridge/jsp/taglib/functions/NodeFunctionTag.java
@@ -16,15 +16,22 @@ import javax.servlet.jsp.JspTagException;
 import org.mmbase.bridge.*;
 import org.mmbase.bridge.jsp.taglib.*;
 import org.mmbase.bridge.jsp.taglib.containers.FunctionContainerReferrer;
+import org.mmbase.bridge.jsp.taglib.util.Attribute;
+import org.mmbase.bridge.jsp.taglib.util.Notfound;
 import org.mmbase.util.Casting;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
 
 /**
  *
  * @author  Michiel Meeuwissen
  * @since   MMBase-1.7
- * @version $Id: NodeFunctionTag.java,v 1.12 2006-07-17 15:38:47 johannes Exp $
+ * @version $Id: NodeFunctionTag.java,v 1.13 2007-08-03 12:07:36 sdeboer Exp $
  */
 public class NodeFunctionTag extends AbstractFunctionTag implements NodeProvider, FunctionContainerReferrer {
+
+    private static final Logger log = Logging.getLoggerInstance(NodeFunctionTag.class);
 
     protected  NodeProviderHelper nodeHelper = new NodeProviderHelper(this); // no m.i. and there are more nodeprovider which cannot extend this, they can use the same trick.
 
@@ -42,6 +49,11 @@ public class NodeFunctionTag extends AbstractFunctionTag implements NodeProvider
         nodeHelper.setNodeVar(node);
     }
 
+    private Attribute notfound = Attribute.NULL;
+
+    public void setNotfound(String i) throws JspTagException {
+        notfound = getAttribute(i);
+    }
 
     protected void fillVars() throws JspTagException {
         nodeHelper.fillVars();
@@ -65,21 +77,41 @@ public class NodeFunctionTag extends AbstractFunctionTag implements NodeProvider
 
     public int doStartTag() throws JspTagException {
         Object value = getFunctionValue(false); // don't register, 'fillVars' will do.
+        if (value == null || "".equals(value)) {
+            switch(Notfound.get(notfound, this)) {
+            case Notfound.MESSAGE:
+                try {
+                    getPageContext().getOut().write("Function value was null");
+                    } catch (java.io.IOException ioe) {
+                        log.warn(ioe);
+                    }
+                    return SKIP_BODY;
+            case Notfound.SKIP:         {
+                return SKIP_BODY;
+            }
+            case Notfound.THROW:  {
+              throw new TaglibException("Function value was null");
+            }
+            default:
+            }
+        }
         Node node = Casting.toNode(value, getCloudVar());
         setNodeVar(node);
         fillVars();
-        return  EVAL_BODY_BUFFERED;
+        return  EVAL_BODY;
     }
 
     /**
      * this method writes the content of the body back to the jsp page
      **/
     public int doAfterBody() throws JspTagException { // write the body if there was one
-        if (bodyContent != null) {
-            try {
-                bodyContent.writeOut(bodyContent.getEnclosingWriter());
-            } catch (IOException ioe){
-                throw new TaglibException(ioe);
+        if (EVAL_BODY == EVAL_BODY_BUFFERED) {
+            if (bodyContent != null) {
+                try {
+                    bodyContent.writeOut(bodyContent.getEnclosingWriter());
+                } catch (IOException ioe){
+                    throw new TaglibException(ioe);
+                }
             }
         }
         return nodeHelper.doAfterBody();
