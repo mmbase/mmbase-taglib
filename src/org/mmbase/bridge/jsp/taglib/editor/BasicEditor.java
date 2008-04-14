6af16270b4a31c95313a8df462a560478210094d
sdeboer <sdeboer@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-04-14 10:16:32 +0000

    oeps: added styleClass param

--- src/org/mmbase/bridge/jsp/taglib/editor/BasicEditor.java
+++ src/org/mmbase/bridge/jsp/taglib/editor/BasicEditor.java
@@ -28,7 +28,7 @@ import org.mmbase.util.logging.Logging;
  * of the very first field the edittag encounters, with an icon to click on.
  *
  * @author Andr&eacute; van Toly
- * @version $Id: BasicEditor.java,v 1.16 2008-04-14 10:07:36 sdeboer Exp $
+ * @version $Id: BasicEditor.java,v 1.17 2008-04-14 10:16:32 sdeboer Exp $
  * @see EditTag
  * @see YAMMEditor
  * @since MMBase-1.8
@@ -108,7 +108,7 @@ public class BasicEditor extends Editor {
                 locale = org.mmbase.bridge.ContextProvider.getDefaultCloudContext().getDefaultLocale();
             }
             String title = ResourceBundle.getBundle("org.mmbase.bridge.jsp.taglib.resources.messages", locale).getString("edit");
-            html.write("<a class=\"" + styleClass + "\" title=\"" + title + "\" href=\"");
+            html.write("<a class=\"" + parameters.getString("styleClass") + "\" title=\"" + title + "\" href=\"");
             html.write(url);
             html.write("\" ");
             if ("new".equals(parameters.getString("target"))) {
