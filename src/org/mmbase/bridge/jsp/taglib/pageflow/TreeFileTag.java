780255f5c53c69741c2da081814e112e24d5fb13
Michiel Meeuwissen <git@meeuw.org>
2008-06-20 15:59:59 +0000

    better compatibility with 1.8

--- src/org/mmbase/bridge/jsp/taglib/pageflow/TreeFileTag.java
+++ src/org/mmbase/bridge/jsp/taglib/pageflow/TreeFileTag.java
@@ -12,6 +12,7 @@ package org.mmbase.bridge.jsp.taglib.pageflow;
 import org.mmbase.bridge.jsp.taglib.TaglibException;
 import org.mmbase.bridge.jsp.taglib.util.Attribute;
 import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.JspException;
 
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
@@ -24,14 +25,14 @@ import org.mmbase.util.logging.Logging;
  * A full description of this command can be found in the mmbase-taglib.xml file.
  *
  * @author Johannes Verelst
- * @version $Id: TreeFileTag.java,v 1.29 2008-02-03 17:33:56 nklasens Exp $
+ * @version $Id: TreeFileTag.java,v 1.30 2008-06-20 15:59:59 michiel Exp $
  */
 
 public class TreeFileTag extends UrlTag {
 
     private static final Logger log = Logging.getLoggerInstance(TreeFileTag.class);
     protected Attribute objectList = Attribute.NULL;
-    protected TreeHelper th = new TreeHelper();
+    protected final TreeHelper th = new TreeHelper();
 
     protected Attribute notFound        = Attribute.NULL;
 
@@ -42,6 +43,7 @@ public class TreeFileTag extends UrlTag {
         notFound = getAttribute(n);
     }
 
+    @Override
     protected String getPage(String p) throws JspTagException {
         try {
             return th.findTreeFile(p, objectList.getValue(this).toString(),
@@ -51,6 +53,7 @@ public class TreeFileTag extends UrlTag {
         }
     }
 
+    @Override
     protected void initTag(boolean internal) throws JspTagException {
         th.setCloud(getCloudVar());
         th.setBackwardsCompatible(! "false".equals(pageContext.getServletContext().getInitParameter("mmbase.taglib.smartpath_backwards_compatible")));
@@ -60,18 +63,32 @@ public class TreeFileTag extends UrlTag {
             log.debug("TreeFile end of starttag: " + url.toString());
         }
     }
-    
 
+    @Override
+    public int doStartTag() throws JspTagException {
+        super.doStartTag();
+        helper.setValue(url);
+        return EVAL_BODY; // lets try _not_ buffering the body.
+        // this may give unexpected results if ${_} is not used (or another tag calling 'haveBody')
+    }
+
+    @Override
     public void doFinally() {
         th.doFinally();
         super.doFinally();
     }
 
 
+    @Override
     // override to cancel
     protected boolean doMakeRelative() {
         log.debug("doMakeRelative() overridden!");
         return false;
+    }
+
+
+    public int doAfterBody() throws JspException {
+        return helper.doAfterBody();
     }
 
 }
