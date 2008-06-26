e1215853b17999a3f626008ef99f963b8e93b806
Michiel Meeuwissen <git@meeuw.org>
2008-06-26 10:18:22 +0000

    added since tag

--- src/org/mmbase/bridge/jsp/taglib/EscapeTag.java
+++ src/org/mmbase/bridge/jsp/taglib/EscapeTag.java
@@ -15,18 +15,19 @@ import org.mmbase.util.logging.Logging;
 /**
 
  * @author Michiel Meeuwissen
- * @version $Id: EscapeTag.java,v 1.2 2008-02-03 17:33:56 nklasens Exp $
+ * @version $Id: EscapeTag.java,v 1.3 2008-06-26 10:18:22 michiel Exp $
+ * @since MMBase-1.8.6
  */
 
 public class EscapeTag extends ContextReferrerTag {
     private static final Logger log = Logging.getLoggerInstance(EscapeTag.class);
 
-    
+
     public int doStartTag() throws JspTagException {
         return EVAL_BODY_BUFFERED;
     }
 
-    public int doEndTag() throws JspTagException {        
+    public int doEndTag() throws JspTagException {
         helper.setValue(bodyContent != null ? bodyContent.getString() : "");
         helper.doEndTag();
         super.doEndTag();
