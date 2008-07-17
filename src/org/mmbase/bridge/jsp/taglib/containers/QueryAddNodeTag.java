7e275c87e720757b12622c0706adf3fc56dd7fcc
Michiel Meeuwissen <git@meeuw.org>
2008-07-17 13:53:16 +0000

    created mm:addnode, the counterpart of Query#addNode

---
+++ src/org/mmbase/bridge/jsp/taglib/containers/QueryAddNodeTag.java
@@ -0,0 +1,58 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib.containers;
+
+import javax.servlet.jsp.JspTagException;
+
+import org.mmbase.bridge.*;
+import org.mmbase.bridge.util.Queries;
+import org.mmbase.bridge.jsp.taglib.ContextReferrerTag;
+import org.mmbase.bridge.jsp.taglib.util.Attribute;
+import org.mmbase.storage.search.*;
+//import org.mmbase.util.logging.*;
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @since  MMBase-1.9
+ * @version $Id: QueryAddNodeTag.java,v 1.1 2008-07-17 13:53:16 michiel Exp $
+ */
+public class QueryAddNodeTag extends ContextReferrerTag implements QueryContainerReferrer {
+
+
+    protected Attribute container  = Attribute.NULL;
+
+    protected Attribute element      = Attribute.NULL;
+    protected Attribute node         = Attribute.NULL;
+
+    public void setContainer(String c) throws JspTagException {
+        container = getAttribute(c);
+    }
+
+    public void setElement(String e) throws JspTagException {
+        element = getAttribute(e);
+    }
+    public void setNumber(String n) throws JspTagException {
+        node = getAttribute(n);
+    }
+
+    public int doStartTag() throws JspTagException {
+        Query query = getQuery(container);
+        Step step = query.getStep(element.getString(this));
+
+        for (String n : node.getList(this)) {
+            query.addNode(step, Integer.parseInt(n));
+        }
+        findWriter(false); // just to call haveBody.., because constraint is not officially a
+                           // writerreferer (but e.g. _ can be used in attributes)
+        return SKIP_BODY;
+    }
+
+}
