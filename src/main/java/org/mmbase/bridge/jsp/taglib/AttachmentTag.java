3f847e153276247cf1694f25e79fc211c361329b
Michiel Meeuwissen <git@meeuw.org>
2009-09-02 09:24:52 +0000

    it makes no sense to add f(asis) to attachment urls

---
+++ src/main/java/org/mmbase/bridge/jsp/taglib/AttachmentTag.java
@@ -0,0 +1,28 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib;
+
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id$
+ */
+
+public class AttachmentTag extends ImageTag {
+
+
+    @Override
+    public String getTemplate(Node node, String t, int widthTemplate, int heightTemplate, String cropTemplate) {
+        return t;
+    }
+
+}
+
