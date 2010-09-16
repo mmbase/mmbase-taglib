b9188092b5b8ce398ee845f2134ffbaf57514016
Michiel Meeuwissen <git@meeuw.org>
2010-09-16 12:26:12 +0000

    just added a @deprecated place  holder to indicate the move

---
+++ src/main/java/org/mmbase/bridge/jsp/taglib/pageflow/TreeHelper.java
@@ -0,0 +1,18 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.bridge.jsp.taglib.pageflow;
+
+
+/**
+ * @deprecated Used org.mmbase.bridge.util.TreeHelper
+ */
+public class TreeHelper extends org.mmbase.bridge.util.TreeHelper {
+
+}
