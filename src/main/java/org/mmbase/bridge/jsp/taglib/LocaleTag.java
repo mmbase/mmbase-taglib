a6d02c970bea619b9c4f6b8e4d9498059b7758b3
Michiel Meeuwissen <git@meeuw.org>
2011-01-14 11:37:15 +0000

    reverted some test which I accidentely test in

--- src/main/java/org/mmbase/bridge/jsp/taglib/LocaleTag.java
+++ src/main/java/org/mmbase/bridge/jsp/taglib/LocaleTag.java
@@ -30,7 +30,7 @@ public class LocaleTag extends CloudReferrerTag  {
     private static final Logger LOG = Logging.getLoggerInstance(LocaleTag.class);
 
     public static final String KEY = "javax.servlet.jsp.jstl.fmt.locale.request";
-    public static final String BIDI_KEY = "org.mmbase.locale.bidi";
+    //public static final String BIDI_KEY = "org.mmbase.locale.bidi";
     public static final String TZ_KEY = "org.mmbase.timezone";
     public static final int SCOPE = PageContext.REQUEST_SCOPE;
     private Attribute language = Attribute.NULL;
@@ -41,7 +41,7 @@ public class LocaleTag extends CloudReferrerTag  {
 
     protected Locale locale;
     protected Locale prevCloudLocale = null;
-    protected Locale prevJstlLocale = null;
+    //protected Locale prevJstlLocale = null;
     protected Cloud  cloud;
     private String jspvar = null;
 
@@ -94,11 +94,11 @@ public class LocaleTag extends CloudReferrerTag  {
             }
             // compatibility with jstl fmt tags:
             // should use their constant, but that would make compile-time dependency.
-            prevJstlLocale = (Locale) pageContext.findAttribute(KEY);
+            //prevJstlLocale = (Locale) pageContext.findAttribute(KEY);
             pageContext.setAttribute(KEY, locale, SCOPE);
-            Bidi bidi = new Bidi(new AttributedString(locale.getDisplayLanguage(locale)).getIterator());
-            LOG.info("Found bidi " + bidi + " from" + locale.getDisplayLanguage(locale));
-            pageContext.setAttribute(BIDI_KEY, bidi, SCOPE);
+            //Bidi bidi = new Bidi(new AttributedString(locale.getDisplayLanguage(locale)).getIterator());
+            //LOG.info("Found bidi " + bidi + " from" + locale.getDisplayLanguage(locale));
+            //pageContext.setAttribute(BIDI_KEY, bidi, SCOPE);
             CloudProvider cloudProvider = findCloudProvider(false);
             if (cloudProvider != null) {
                 cloud = cloudProvider.getCloudVar();
@@ -193,6 +193,7 @@ public class LocaleTag extends CloudReferrerTag  {
                 }
             }
 
+            /*
             if (prevJstlLocale != null) {
                 pageContext.setAttribute(KEY, prevJstlLocale, SCOPE);
                 pageContext.setAttribute(BIDI_KEY, new Bidi(new AttributedString(prevJstlLocale.getDisplayLanguage(prevJstlLocale)).getIterator()), SCOPE);
@@ -200,6 +201,7 @@ public class LocaleTag extends CloudReferrerTag  {
                 pageContext.removeAttribute(KEY, SCOPE);
                 pageContext.removeAttribute(BIDI_KEY, SCOPE);
             }
+            */
         }
         cloud = null;
         varyHeaders = null;
@@ -211,7 +213,7 @@ public class LocaleTag extends CloudReferrerTag  {
         cloud = null;
         locale = null;
         prevCloudLocale = null;
-        prevJstlLocale = null;
+        //prevJstlLocale = null;
         jspvar = null;
         super.doFinally();
     }
